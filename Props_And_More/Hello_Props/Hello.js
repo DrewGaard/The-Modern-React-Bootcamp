a0971527f0009bd83f01c50463144fe6846a0659
Drew <d_overgaard1@u.pacific.edu>
2019-04-21 17:46:36 -0700

    Learning about more React Propts

--- Props_And_More/Hello_Props/Hello.js
+++ Props_And_More/Hello_Props/Hello.js
@@ -1,5 +1,12 @@
 class Hello extends React.Component {
 	render() {
-		return <h1>Hello There!!! From: {this.props.from} To: {this.props.to}</h1>
+        let bangs = "!".repeat(this.props.bangs);
+        console.log(this.props);
+		return (
+        <div>
+            <h1>Hello There!!! From: {this.props.from} To: {this.props.to}{bangs}</h1>
+            <img src={this.props.img} />
+        </div>
+        )
 	}
 }
--- Props_And_More/Hello_Props/index.js
+++ Props_And_More/Hello_Props/index.js
@@ -2,8 +2,12 @@ class App extends React.Component {
 	render() {
 		return (
             <div>
-                <Hello to="Ringo" from="Paul"/>
-                <Hello to="Cher" from="Sonny"/>
+                <Hello 
+                    to="Ringo" 
+                    from="Paul"
+                    bangs={4}
+                    img="https://images.unsplash.com/photo-1516956566-e9fbebf4842f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+                />
             </div>
         )
 	}
