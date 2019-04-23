2080c7984c6f2eb1240b87a23c64cfc94df103a8
Drew <d_overgaard1@u.pacific.edu>
2019-04-23 12:57:55 -0700

    Completed the Pokedex Project

--- Pokedex_Project/pokedex/src/App.js
+++ Pokedex_Project/pokedex/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Pokedex from './pokedex.js'
+import Pokegame from './pokegame';
 
 function App() {
   return (
     <div className="App">
-      <Pokedex/>
+      <Pokegame/>
     </div>
   );
 }
