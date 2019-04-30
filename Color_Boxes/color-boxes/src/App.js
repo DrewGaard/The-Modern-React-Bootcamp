d984aec98f9a7f65d5b9aae2f361f464f898eef0
Drew <d_overgaard1@u.pacific.edu>
2019-04-29 18:12:27 -0700

    Making the Color Boxes project

---
+++ Color_Boxes/color-boxes/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Box from "./Box";
+import BoxContainer from "./BoxContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <BoxContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Color_Boxes/color-boxes/src/Box.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import "./Box.css";
+import { choice } from "./helpers";
+
+class Box extends Component {
+    static defaultProps = {
+        allColors: ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"]
+    };
+    constructor(props) {
+        super(props);
+        this.state = {color: choice(this.props.colors)};
+        this.handleClick = this.handleClick.bind(this);
+    }
+    pickColor() {
+        let newColor;
+        do {
+            newColor = choice(this.props.allColors);
+        } while (newColor === this.state.color)
+        this.setState({ color: newColor });
+    }
+
+    handleClick() {
+        this.pickColor();
+    }
+    render() {
+        return (
+            <div className="Box" style={{backgroundColor: this.state.color}} onClick={this.handleClick}>
+                
+            </div>
+        );
+    }
+}
+
+export default Box;
